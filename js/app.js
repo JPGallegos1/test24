56ae10c79132f7b493e298023417f30792f15525
Juan Pablo Gallegos <jpgallegos20gmail.com>
2020-01-19 07:02:21 -0300

    I implemented the hamburguer menu but I'm need to make some fixes

--- js/app.js
+++ js/app.js
@@ -4,3 +4,25 @@ btnDark.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnDark.classList.toggle('active');
 });
+
+const html = document.querySelector("html");
+const nav = document.querySelector(".navigation");
+const hamburguerMenu = document.querySelector(".hamburguer-menu");
+
+const navigationIsOpen = () => html.classList.contains("nav-is-open");
+
+const openNav = () => {
+    html.classList.add("nav-is-open");
+}
+
+const closeNav = () => {
+    html.classList.remove("nav-is-open");
+}
+
+const toggleNav = () => {
+    navigationIsOpen() ? closeNav() : openNav();
+}
+
+hamburguerMenu.addEventListener('click', () => {
+    toggleNav();
+})
